961ac39d2b33f1ecccc4355e48fc3628c988f14e
vpsong <songguibin@yeah.net>
2014-01-07 00:39:27 +0800

    first commit

---
+++ src/main/vp/freez/web/servlet/FreezServlet.java
@@ -0,0 +1,29 @@
+package vp.freez.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * @author vpsong
+ *
+ */
+public class FreezServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public FreezServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
